442e04dcdaae20b114d02c241e324ac13cc222a5
trrmann <113477307+trrmann@users.noreply.github.com>
2026-01-14 12:30:26 -0500

    added vite to build and package

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: '.',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+  server: {
+    open: 'index.html',
+  },
+});
